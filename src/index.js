bcb33af8c10bc1a217afe7d48e620ed02bfa6f7d
红尘渡者 <hongchenduzhe00@163.com>
2026-01-02 18:50:10 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -847,7 +847,7 @@ async function postSubmit(comment) {
 // 将评论转为数据库存储格式
 async function parse(comment, request) {
 	const timestamp = Date.now()
-	// const isAdminUser = isAdmin()
+	const isAdminUser = isAdmin()
 	const isBloggerMail = equalsMail(comment.mail, config.BLOGGER_EMAIL)
 	// if (isBloggerMail && !isAdminUser) throw new Error('请先登录管理面板，再使用博主身份发送评论')
 	const hashMethod = config.GRAVATAR_CDN === 'cravatar.cn' ? md5 : sha256
